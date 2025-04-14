c178e86e08d03eb26def47ffc8579748f7c502d8
balumaqsud <ollobergan008@mail.ru>
2025-04-14 15:07:16 +0900

    feat: material ui tested

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "../css/app.css";
+import { Container, Stack, Box, Typography, Button } from "@mui/material";
 
 function App() {
-  return <div>HOME</div>;
+  return (
+    <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component={"h4"}>
+            This is material UI with React
+          </Typography>
+        </Box>
+        <Button variant="contained">material ui</Button>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
-
-
-
-
-
