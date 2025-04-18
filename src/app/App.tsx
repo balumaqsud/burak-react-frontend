c64f58b573ea7129923a601f16ec5d1d9c688439
balumaqsud <ollobergan008@mail.ru>
2025-04-18 16:42:36 +0900

    feat: nav and footer created

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,34 +1,20 @@
 import React from "react";
 import "../css/app.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link, useLocation } from "react-router-dom";
 import HomePage from "./screens/homePage";
 import UsersPage from "./screens/usersPage";
 import ProductsPage from "./screens/productsPage";
 import HelpPage from "./screens/helpPage";
 import OrdersPage from "./screens/ordersPage";
+import OtherNavbar from "./components/headers/otherNavbar";
+import HomeNavbar from "./components/headers/homeNavbar";
+import Footer from "./components/footer";
 
 function App() {
+  const location = useLocation();
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-          <li>
-            <Link to="/orders">Orders</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-          <li>
-            <Link to="/help">Help</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
       <Switch>
         <Route path="/orders">
           <OrdersPage />
@@ -46,7 +32,8 @@ function App() {
           <HomePage />
         </Route>
       </Switch>
-    </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/app/components/headers/otherNavbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OtherNavbar = () => {
+  return <div>otherNavbar</div>;
+};
+
+export default OtherNavbar;
