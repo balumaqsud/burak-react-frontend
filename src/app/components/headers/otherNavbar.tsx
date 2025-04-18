7349af9e06a681ad42a3e35ffd76cfcb18e7412a
balumaqsud <ollobergan008@mail.ru>
2025-04-18 19:52:17 +0900

    feat: other navbar header component

--- src/app/components/headers/otherNavbar.tsx
+++ src/app/components/headers/otherNavbar.tsx
@@ -1,9 +1,58 @@
-import { Container } from "@mui/material";
+import { Box, Button, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 export const OtherNavbar = () => {
+  const authMember = null;
   return (
-    <div>
-      <Container>other</Container>
+    <div className="other-navbar">
+      <Container className="navbar-container">
+        <Stack className="menu">
+          <Box>
+            <NavLink to="/">
+              <img
+                src="/icons/burak.svg"
+                style={{ width: "125px", height: "30px" }}
+                alt="burak logo"
+              />
+            </NavLink>
+          </Box>
+          <Stack className="links">
+            <Box className="hover-line">
+              <NavLink to="/">Home</NavLink>
+            </Box>
+            <Box className="hover-line">
+              <NavLink to="/products">Products</NavLink>
+            </Box>
+            {authMember ? (
+              <Box className="hover-line">
+                <NavLink to="/orders">Orders</NavLink>
+              </Box>
+            ) : null}
+            {authMember ? (
+              <Box className="hover-line">
+                <NavLink to="/member-page">My Page</NavLink>
+              </Box>
+            ) : null}
+            <Box className="hover-line">
+              <NavLink to="/help">Help</NavLink>
+            </Box>
+            {!authMember ? (
+              <Box>
+                <Button variant="contained" className="login-button">
+                  Login
+                </Button>
+              </Box>
+            ) : (
+              <img
+                className="user-avatar"
+                src={"/icons/default-user.svg"}
+                aria-haspopup={"true"}
+                alt=""
+              />
+            )}
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 };
