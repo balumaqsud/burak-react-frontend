0ee6993a5f039485ea12e1acb65979673b11bf7e
balumaqsud <ollobergan008@mail.ru>
2025-04-23 18:27:35 +0900

    fix: products page modified

--- src/app/screens/productsPage/Products.tsx
+++ src/app/screens/productsPage/Products.tsx
@@ -135,8 +135,39 @@ const Products = () => {
           </Stack>
         </Stack>
       </Container>
-      <div className="brands-logo"></div>
-      <div className="address"></div>
+      <div className="brands-logo">
+        <Container className={"brands"}>
+          <Box className={"category-title"}>Our family brands</Box>
+          <Stack className={"brand-list"}>
+            <Box className={"review-box"}>
+              <img src={"/img/gurme.webp"} alt="" />
+            </Box>
+            <Box className={"review-box"}>
+              <img src={"/img/sweets.webp"} alt="" />
+            </Box>
+            <Box className={"review-box"}>
+              <img src={"/img/seafood.webp"} alt="" />
+            </Box>
+            <Box className={"review-box"}>
+              <img src={"/img/doner.webp"} alt="" />
+            </Box>
+          </Stack>
+        </Container>
+      </div>
+      <div className="address">
+        <Container>
+          <Stack className={"address-area"}>
+            <Box className={"title"}>Our address</Box>
+            <iframe
+              style={{ marginTop: "60px" }}
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.363734762081!2d69.2267250514616!3d41.322703307863044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b9a0a33281d%3A0x9c5015eab678e435!2z0KDQsNC50YXQvtC9!5e0!3m2!1sko!2skr!4v1655461169573!5m2!1sko!2skr"
+              width="1320"
+              height="500"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </Stack>
+        </Container>
+      </div>
     </div>
   );
 };
