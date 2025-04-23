c123449682d3d93393bc60563bf2d15203e10822
balumaqsud <ollobergan008@mail.ru>
2025-04-23 12:12:36 +0900

    feat: new dishes sectional component created

--- src/app/screens/homePage/NewDIshes.tsx
+++ src/app/screens/homePage/NewDIshes.tsx
@@ -1,10 +1,63 @@
-import { Container } from "@mui/material";
 import React from "react";
+import { Box, Container, Stack } from "@mui/material";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Card from "@mui/joy/Card";
+import CardOverflow from "@mui/joy/CardOverflow";
+import Typography from "@mui/joy/Typography";
+import { CssVarsProvider } from "@mui/joy/styles";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import Divider from "../../components/divider";
+
+const newDishes = [
+  { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
+  { productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
+  { productName: "Kebab", imagePath: "/img/kebab.webp" },
+  { productName: "Lavash", imagePath: "/img/lavash.webp" },
+];
 
 const NewDishes = () => {
   return (
-    <div>
-      <Container>NewDishes</Container>
+    <div className="new-products-frame">
+      <Container>
+        <Stack className="main">
+          <Box className="dishes_title">New Dishes</Box>
+          <Stack className="cards-frame">
+            <CssVarsProvider>
+              {newDishes.map((ele, index) => {
+                return (
+                  <Card key={index} variant="outlined" className="card">
+                    <CardOverflow>
+                      <div className="product-sale">Normal size</div>
+                      <AspectRatio ratio="1">
+                        <img src={ele.imagePath} alt="" />
+                      </AspectRatio>
+                    </CardOverflow>
+                    <CardOverflow variant="soft" className="product-detail">
+                      <Stack className="info">
+                        <Stack flexDirection="row">
+                          <Typography className="title">
+                            {ele.productName}
+                          </Typography>
+                          <Divider width="2" height="24" bg="#d9d9d9" />
+                          <Typography className="price">$12</Typography>
+                        </Stack>
+                        <Stack>
+                          <Typography className="views">
+                            20
+                            <VisibilityIcon
+                              sx={{ fontSize: 20, marginLeft: "5px" }}
+                            />
+                          </Typography>
+                        </Stack>
+                      </Stack>
+                    </CardOverflow>
+                  </Card>
+                );
+              })}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 };
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Statistics from "./statistics";
 import PopularDishes from "./PopularDishes";
-import NewDishes from "./PopularDishes copy";
+import NewDishes from "./NewDIshes";
 import Advertisements from "./Advertisements";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
