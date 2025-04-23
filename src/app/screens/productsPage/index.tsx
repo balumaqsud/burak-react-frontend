496e3fe63bb50ce61c26d898429723ec35c2194e
balumaqsud <ollobergan008@mail.ru>
2025-04-23 15:03:17 +0900

    feat: products page: route system implemented

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,10 +1,21 @@
 import { Container } from "@mui/material";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
+import ChosenProduct from "./ChosenProduct";
 import React from "react";
+import Products from "./Products";
 
 const ProductsPage = () => {
   const products = useRouteMatch();
-  return <Container>ProductsPage</Container>;
+  return (
+    <Switch>
+      <Route path={`${products.path}/:productID`}>
+        <ChosenProduct />
+      </Route>
+      <Route path={`${products.path}`}>
+        <Products />
+      </Route>
+    </Switch>
+  );
 };
 
 export default ProductsPage;
