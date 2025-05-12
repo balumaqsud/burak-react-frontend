2b802afb04330c0cf5fc4e15fe0de9d9890736aa
balumaqsud <ollobergan008@mail.ru>
2025-05-12 17:42:00 +0900

    developed homage redux config

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./stats";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDIshes";
@@ -8,6 +8,12 @@ import Events from "./Events";
 import "../../../css/home.css";
 
 const HomePage = () => {
+  //selector: store => data
+
+  useEffect(() => {
+    //Backend server data request = Data
+    //Slice: Data => Store
+  }, []);
   return (
     <div className="homepage">
       <Statistics />
---
+++ src/app/screens/homePage/selector.ts.ts
@@ -0,0 +1,17 @@
+import { createSelector } from "reselect";
+import { AppRootState } from "../../../lib/types/screen";
+
+const selectHomePage = (state: AppRootState) => state.homepage;
+
+export const retrievePopularDishes = createSelector(
+  selectHomePage,
+  (Homepage) => Homepage.popularDishes
+);
+export const retrieveNewDishes = createSelector(
+  selectHomePage,
+  (Homepage) => Homepage.newDishes
+);
+export const retrieveTopUsers = createSelector(
+  selectHomePage,
+  (Homepage) => Homepage.topUsers
+);
