ee25c82b3368d8198f25185fd7ed3cb2ddc55d8b
balumaqsud <ollobergan008@mail.ru>
2025-05-23 19:21:02 +0900

    feat: user page updated

--- src/app/screens/usersPage/index.tsx
+++ src/app/screens/usersPage/index.tsx
@@ -6,6 +6,8 @@ import YouTubeIcon from "@mui/icons-material/YouTube";
 import { Settings } from "./Settings";
 import { useHistory } from "react-router-dom";
 import { useGlobals } from "../../hooks/useGlobals";
+import { serverApi } from "../../../lib/config";
+import { MemberType } from "../../../lib/data/enums/member.enum";
 
 import "../../../css/userPage.css";
 
@@ -35,17 +37,38 @@ export default function UserPage() {
               >
                 <div className={"order-user-img"}>
                   <img
-                    src={"/icons/default-user.svg"}
+                    src={
+                      authMember?.memberImage
+                        ? `${serverApi}/${authMember.memberImage}`
+                        : "/icons/default-user.svg"
+                    }
                     className={"order-user-avatar"}
                     alt=""
                   />
                   <div className={"order-user-icon-box"}>
-                    <img src={"/icons/user-badge.svg"} alt="" />
+                    <img
+                      src={
+                        authMember?.memberType === MemberType.RESTAURANT
+                          ? "/icons/restaurant.svg"
+                          : "/icons/user-badge.svg"
+                      }
+                      alt=""
+                    />
                   </div>
                 </div>
-                <span className={"order-user-name"}>Martin</span>
-                <span className={"order-user-prof"}>User</span>
-                <span className={"order-user-prof"}>No address</span>
+                <span className={"order-user-name"}>
+                  {authMember?.memberNick}
+                </span>
+                <span className={"order-user-prof"}>
+                  {" "}
+                  {authMember?.memberType}
+                </span>
+                <span className={"order-user-prof"}>
+                  {" "}
+                  {authMember?.memberAddress
+                    ? authMember.memberAddress
+                    : "no address"}
+                </span>
               </Box>
               <Box className={"user-media-box"}>
                 <FacebookIcon />
@@ -53,7 +76,11 @@ export default function UserPage() {
                 <TelegramIcon />
                 <YouTubeIcon />
               </Box>
-              <p className={"user-desc"}>No description</p>
+              <p className={"user-desc"}>
+                {authMember?.memberDescription
+                  ? authMember.memberDescription
+                  : "No description"}
+              </p>
             </Box>
           </Stack>
         </Stack>
